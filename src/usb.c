a44b01ca5750a0fa3be08c3fb2044b39af362c35
Alexander Gherardi <alexander.2003@live.com>
2020-08-24 16:38:25 -0400

    Remove dtr wait

--- src/usb.c
+++ src/usb.c
@@ -114,28 +114,10 @@ void write_usb(struct device *dev, uint8_t op, uint8_t *buf, int len)
 
 void init_usb(void)
 {
-    // Usb vars
-    uint32_t dtr = 0U;
     // Get cdc device
     usb = device_get_binding("CDC_ACM_0");
     // Enable usb
     usb_enable(NULL);
-    // Wait for dtr
-    while (true)
-    {
-        uart_line_ctrl_get(usb, UART_LINE_CTRL_DTR, &dtr);
-        if (dtr)
-        {
-            break;
-        }
-        else
-        {
-            k_sleep(K_MSEC(100));
-        }
-    }
-    uart_line_ctrl_set(usb, UART_LINE_CTRL_DCD, 1);
-    uart_line_ctrl_set(usb, UART_LINE_CTRL_DSR, 1);
-    k_busy_wait(1000000);
     // Set callback
     uart_irq_callback_set(usb, handler);
     // Enable interrupts
